ca7950eca523da40698e368cff609b8d357677d6
Greli <greli.experiencia@gmail.com>
2017-03-22 00:11:24 -0500

    V2 Minor changes
    Hurry up!

--- js/init.js
+++ js/init.js
@@ -1,8 +1,42 @@
-(function($){
-  $(function(){
+(function($) {
+    $(function() {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+        $('.carousel').carousel();
+        $('.carousel').carousel({
+            padding: 200
+        });
+        autoplay()
 
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-
-  }); // end of document ready
+        function autoplay() {
+            $('.carousel').carousel('next');
+            setTimeout(autoplay, 2500);
+        }
+        $('.target').pushpin({
+            top: 0
+        });
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
+$('.menu-flexible').each(function() {
+    var $this = $(this);
+    var $target = $('#' + $(this).attr('data-target'));
+    $this.pushpin({
+        top: $target.offset().top,
+        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+    });
+});
+var options = [{
+    selector: '#contactanos',
+    offset: 50,
+    callback: function(el) {
+        Materialize.toast("Contactanos via whatsapp al 3002386570", 1500);
+    }
+}, {
+    selector: '#salir-de-la-rutina',
+    offset: 205,
+    callback: function(el) {
+      Materialize.toast("Recuerda que te obsequiaremos una bebida inicial", 1500);
+        
+    }
+}];
+Materialize.scrollFire(options);
