d5e8bc74cd1ce0d245272e3470f1487323e52d2c
Greli <greli.experiencia@gmail.com>
2017-03-25 15:12:29 -0500

    Several adjustments
    Font text, footer parallax, and more

--- js/init.js
+++ js/init.js
@@ -1,6 +1,12 @@
 (function($) {
     $(function() {
         $('.button-collapse').sideNav();
+        $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 300
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+            draggable: true // Choose whether you can drag to open on touch screens
+        });
         $('.parallax').parallax();
         $('.carousel').carousel();
         $('.carousel').carousel({
@@ -35,8 +41,8 @@ var options = [{
     selector: '#salir-de-la-rutina',
     offset: 205,
     callback: function(el) {
-      Materialize.toast("Recuerda! Te obsequiaremos una bebida inicial", 5000);
-        
+        Materialize.toast("Recuerda! Te obsequiaremos una bebida inicial", 5000);
+
     }
 }];
 Materialize.scrollFire(options);
